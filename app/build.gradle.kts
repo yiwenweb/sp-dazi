4c77ee78a5e170da70a9ebb89bfc7ef0d811c6a6
SunnyPilot Toolbox <toolbox@example.com>
2026-07-04 20:29:34 +0800

    fix: add material-icons-extended and Color import in Theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.extended)
     implementation(libs.jsch)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
